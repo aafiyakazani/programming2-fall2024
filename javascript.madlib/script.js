4972c1002a3510fb5284c066234cf25179633099
Aafiya Kazani <aafiyakazani@Aafiyas-MacBook-Air.local>
2024-11-22 11:51:56 -0600

    javascript

---
+++ javascript.madlib/script.js
@@ -0,0 +1,11 @@
+function generatemadlib() {
+    const a1= document.getElementById("a1input").value;
+    const a2= document.getElementById("a2input").value;
+    const a3= document.getElementById("a3input").value;
+    
+    document.getElementById("a1").innerText=a1;
+    document.getElementById("a2").innerText=a2;
+    document.getElementById("a3").innerText=a3;
+    
+    
+}
