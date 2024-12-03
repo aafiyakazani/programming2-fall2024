aadb6acdb0b78d7e0cb9b25c21cb4a2ae529fa52
Aafiya Kazani <aafiyakazani@Aafiyas-MacBook-Air.local>
2024-12-03 10:25:29 -0600

    name-generstor

---
+++ name-generator/script.js
@@ -0,0 +1,89 @@
+
+
+function generate() {
+    let a1input=document.GetElementById("a1input").value;
+    
+    if (a1input=="A" || a1input=="a"){
+        alert("Opal");
+    }
+    else if (a1input=="B" || a1input=="b"){
+        alert("petal");
+    }
+    else if (a1input=="C" || a1input=="c"){
+        alert("");
+    }
+    else if (a1input=="D" || a1input=="d"){
+        alert("petal");
+    }
+    else if (a1input=="E" || a1input=="e"){
+        alert("petal");
+    }
+    else if (a1input=="F" || a1input=="f"){
+        alert("petal");
+    }
+    else if (a1input=="G" || a1input=="g"){
+        alert("petal");
+    }
+    else if (a1input=="H" || a1input=="h"){
+        alert("petal");
+    }
+    else if (a1input=="I" || a1input=="i"){
+        alert("petal");
+    }
+    else if (a1input=="J" || a1input=="j"){
+        alert("petal");
+    }
+    else if (a1input=="K" || a1input=="k"){
+        alert("petal");
+    }
+    else if (a1input=="L" || a1input=="l"){
+        alert("petal");
+    }
+   else if (a1input=="M" || a1input=="m"){
+        alert("petal");
+    }
+    else if (a1input=="N" || a1input=="n"){
+        alert("petal");
+    }
+    else if (a1input=="O" || a1input=="o"){
+        alert("petal");
+    }
+    else if (a1input=="P" || a1input=="p"){
+        alert("petal");
+    }
+    else if (a1input=="Q" || a1input=="q"){
+        alert("petal");
+    }
+    else if (a1input=="R" || a1input=="r"){
+        alert("petal");
+    }
+    else if (a1input=="S" || a1input=="s"){
+        alert("petal");
+    }
+    else if (a1input=="T" || a1input=="t"){
+        alert("petal");
+    }
+    else if (a1input=="U" || a1input=="u"){
+        alert("petal");
+    }
+    else if (a1input=="V" || a1input=="v"){
+        alert("petal");
+    }
+    else if (a1input=="W" || a1input=="w"){
+        alert("petal");
+    }
+    else if (a1input=="X" || a1input=="x"){
+        alert("petal");
+    }
+    else if (a1input=="Y" || a1input=="y"){
+        alert("petal");
+    }
+    else if (a1input=="Z" || a1input=="z"){
+        alert("petal");
+    }
+    else {
+        alert("enter a valid input please");
+    }
+    
+    if (age)
+}
