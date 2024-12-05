dab0c8be54811954c66f30d53a1303ba2c6b295c
Aafiya Kazani <aafiyakazani@Aafiyas-MacBook-Air.local>
2024-12-05 15:20:29 -0600

    name-generator

--- name-generator/script.js
+++ name-generator/script.js
@@ -1,89 +1,116 @@
-
-
 function generate() {
-    let a1input=document.GetElementById("a1input").value;
+    let a1input = document.getElementById("a1input").value.toLowerCase();  
+    let month = document.getElementById("month").value;  
+    let age = parseInt(document.getElementById("age").value);  
+
+    let firstName = "";
+    if (a1input === "a") {
+        firstName = "Opal";
+    } else if (a1input === "b") {
+        firstName = "Petal";
+    } else if (a1input === "c") {
+        firstName = "Cloud";
+    } else if (a1input === "d") {
+        firstName = "Daisy";
+    } else if (a1input === "e") {
+        firstName = "Eclipse";
+    } else if (a1input === "f") {
+        firstName = "Fern";
+    } else if (a1input === "g") {
+        firstName = "Ginger";
+    } else if (a1input === "h") {
+        firstName = "Holly";
+    } else if (a1input === "i") {
+        firstName = "Ivory";
+    } else if (a1input === "j") {
+        firstName = "Jade";
+    } else if (a1input === "k") {
+        firstName = "Kale";
+    } else if (a1input === "l") {
+        firstName = "Lily";
+    } else if (a1input === "m") {
+        firstName = "Magnolia";
+    } else if (a1input === "n") {
+        firstName = "Nash";
+    } else if (a1input === "o") {
+        firstName = "Olive";
+    } else if (a1input === "p") {
+        firstName = "Poppy";
+    } else if (a1input === "q") {
+        firstName = "Quartz";
+    } else if (a1input === "r") {
+        firstName = "Rose";
+    } else if (a1input === "s") {
+        firstName = "Sunflower";
+    } else if (a1input === "t") {
+        firstName = "Tulip";
+    } else if (a1input === "u") {
+        firstName = "Ursula";
+    } else if (a1input === "v") {
+        firstName = "Violet";
+    } else if (a1input === "w") {
+        firstName = "Wisteria";
+    } else if (a1input === "x") {
+        firstName = "Xander";
+    } else if (a1input === "y") {
+        firstName = "Yasmine";
+    } else if (a1input === "z") {
+        firstName = "Zinnia";
+    } else {
+        alert("Please enter a valid input for the first name.");
+        return;
+    }
+
+    let nickname = "";
+    if (age > 0 && age < 10) {
+        nickname = "Buck";
+    } else if (age >= 10 && age <= 20) {
+        nickname = "Tater";
+    } else if (age >= 30 && age <= 40) {
+        nickname = "Rusty";
+    } else if (age >= 40 && age <= 50) {
+        nickname = "Chip";
+    } else if (age > 50) {
+        nickname = "Bubbles";
+    } else {
+        nickname = "Penny";
+    }
+
     
-    if (a1input=="A" || a1input=="a"){
-        alert("Opal");
+    let lastName = "";
+    if (month === "January") {
+        lastName = "Nasty";
+    } else if (month === "February") {
+        lastName = "Tot";
+    } else if (month === "March") {
+        lastName = "Shackleford";
+    } else if (month === "April") {
+        lastName = "Munk";
+    } else if (month === "May") {
+        lastName = "McGraw";
+    } else if (month === "June") {
+        lastName = "Loafer";
+    } else if (month === "July") {
+        lastName = "Bacon";
+    } else if (month === "August") {
+        lastName = "O'Problem";
+    } else if (month === "September") {
+        lastName = "Natic";
+    } else if (month === "October") {
+        lastName = "Matic";
+    } else if (month === "November") {
+        lastName = "O'Furniture";
+    } else if (month === "December") {
+        lastName = "Pigg";
+    } else {
+        alert("Please enter a valid month.");
+        return;
     }
-    else if (a1input=="B" || a1input=="b"){
-        alert("petal");
-    }
-    else if (a1input=="C" || a1input=="c"){
-        alert("");
-    }
-    else if (a1input=="D" || a1input=="d"){
-        alert("petal");
-    }
-    else if (a1input=="E" || a1input=="e"){
-        alert("petal");
-    }
-    else if (a1input=="F" || a1input=="f"){
-        alert("petal");
-    }
-    else if (a1input=="G" || a1input=="g"){
-        alert("petal");
-    }
-    else if (a1input=="H" || a1input=="h"){
-        alert("petal");
-    }
-    else if (a1input=="I" || a1input=="i"){
-        alert("petal");
-    }
-    else if (a1input=="J" || a1input=="j"){
-        alert("petal");
-    }
-    else if (a1input=="K" || a1input=="k"){
-        alert("petal");
-    }
-    else if (a1input=="L" || a1input=="l"){
-        alert("petal");
-    }
-   else if (a1input=="M" || a1input=="m"){
-        alert("petal");
-    }
-    else if (a1input=="N" || a1input=="n"){
-        alert("petal");
-    }
-    else if (a1input=="O" || a1input=="o"){
-        alert("petal");
-    }
-    else if (a1input=="P" || a1input=="p"){
-        alert("petal");
-    }
-    else if (a1input=="Q" || a1input=="q"){
-        alert("petal");
-    }
-    else if (a1input=="R" || a1input=="r"){
-        alert("petal");
-    }
-    else if (a1input=="S" || a1input=="s"){
-        alert("petal");
-    }
-    else if (a1input=="T" || a1input=="t"){
-        alert("petal");
-    }
-    else if (a1input=="U" || a1input=="u"){
-        alert("petal");
-    }
-    else if (a1input=="V" || a1input=="v"){
-        alert("petal");
-    }
-    else if (a1input=="W" || a1input=="w"){
-        alert("petal");
-    }
-    else if (a1input=="X" || a1input=="x"){
-        alert("petal");
-    }
-    else if (a1input=="Y" || a1input=="y"){
-        alert("petal");
-    }
-    else if (a1input=="Z" || a1input=="z"){
-        alert("petal");
-    }
-    else {
-        alert("enter a valid input please");
-    }
+
+    let fullName = firstName + " " + nickname + " " + lastName;
     
-    if (age)
+    alert("Your funny name is: " + fullName);
 }
+
+
+
