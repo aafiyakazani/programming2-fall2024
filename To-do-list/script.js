e3b7fea128c9985af8c63adcc4e101dac91add59
Aafiya Kazani <aafiyakazani@Aafiyas-MacBook-Air.local>
2024-12-12 08:49:59 -0600

    todolist

---
+++ To-do-list/script.js
@@ -0,0 +1,57 @@
+let tasks = ["do laundry","wash dishes"];
+
+function showtasks(){
+    let tasklistUL = document.getElementById("task-list");
+    tasklistUL.innerHTML= "";
+    for (let i=0; i<tasks.length; i++){
+        let li =document.createElement("li");
+        li.innerText=tasks[i];
+        tasklistUL.appendChild(li);
+    }
+}
+
+function addTasks(){
+    let task = prompt("enter your tasks");
+    tasks.push(task);
+    showtasks();
+    
+}
+
+function removeTasks(){
+    let task = prompt("remove your tasks");
+     let taskIndex = tasks.indexOf(taskToRemove);
+    if (taskIndex !== -1) {
+        tasks.splice(taskIndex, 1); 
+        showTasks();  
+    }
+    else {
+        alert("enter a valid task !");
+    }
+}
+
+
+function clearTasks() {
+    tasks = [];
+    showTasks();
+}
+
+function prioritizeTasks() {
+    let taskToPrioritize = prompt("Enter the task you want to prioritize:");
+    let taskFound = false;
+    let tasklist = document.getElementById("task-list");
+     let listItems = taskList.getElementsByTagName("li");
+    tasks = tasks.toLowerCase();
+
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].toLowerCase() === task) {
+            listItems[i].style.color = "yellow";
+            taskFound = true;
+        }
+    }
+    if (!taskFound) {
+        alert("task not found");
+    }
+}
+
+    
+ 
