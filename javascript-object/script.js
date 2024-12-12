f997830c8e0f061c9a75cd59f19decc40a51538b
Aafiya Kazani <aafiyakazani@Aafiyas-MacBook-Air.local>
2024-12-12 09:35:00 -0600

    object

---
+++ javascript-object/script.js
@@ -0,0 +1,24 @@
+let person ={
+    name: "terry",
+    age:23,
+    favoriteColor: "green",
+    height: "6'8\"",
+    satscore:400,
+    job: "basketball player",
+    biggestfear: "inflated balls",
+    favoriteno: 3
+    
+};
+
+let person2 = {
+    name:"joy",
+    age:28,
+    favoriteColor: "red",
+    height: "5'4\"",
+    satscore:256,
+    job: "teacher",
+    biggestfear: "lizards ",
+    favoriteno: 4
+};
+
+console.log("once apon a time there were 2 brothers," + person.name +","+person2.name+"one day both of them went for a job application where they were asked some of their details"+ person.name+ "listed his details as"+"age:"+person.age+"satscore:"+person.satscore + "so did"+person2.name+"his details were"+"age:"+person2.age+"satscore:"+person2.satscore+ "but unfortunately, none of them made it because"+ person.name+ "had his sat score greater than expected and "+ person2.name + "had his score lesser than expected:("+"so sad " );
